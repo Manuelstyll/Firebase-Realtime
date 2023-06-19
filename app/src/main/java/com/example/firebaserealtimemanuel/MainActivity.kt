03875c3723c5ae0ebe6ca914339868c50808e164
Manuelstyll <relmvrelm@gmail.com>
2023-06-19 09:56:08 +0300

    initial commit

---
+++ app/src/main/java/com/example/firebaserealtimemanuel/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.firebaserealtimemanuel
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.example.firebaserealtimemanuel.FetchingActivity
+import com.example.firebaserealtimemanuel.InsertionActivity
+import com.example.firebaserealtimemanuel.R
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnInsertData: Button
+    private lateinit var btnFetchData: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnInsertData = findViewById(R.id.btnInsertData)
+        btnFetchData = findViewById(R.id.btnFetchData)
+
+        btnInsertData.setOnClickListener {
+            val intent = Intent(this, InsertionActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnFetchData.setOnClickListener {
+            val intent = Intent(this, FetchingActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
